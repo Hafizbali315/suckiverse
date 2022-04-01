4e120e1259b2da1c9b6215756b3e03313ae15022
hafizbali315 <hafizbali315@gmail.com>
2022-04-02 04:29:37 +0500

    Completed

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import Navbar from './components/Navbar'
 import Mission from './components/Mission'
 import Minting from './components/Minting'
 import Tokenomics from './components/Tokenomics'
+import Suckmap from './components/Suckmap'
+
+import BackgroundImg from './assets/BackgroundImg.png'
 
 function App() {
 	return (
 		<div className="App">
+			<div className="background_img">
+				<img src={BackgroundImg} alt="BackgroundImg" />
+			</div>
+
 			<Navbar />
 			<Mission />
 			<Minting />
 			<Tokenomics />
+			<Suckmap />
 		</div>
 	)
 }
---
+++ src/components/Suckmap.js
@@ -0,0 +1,30 @@
+const Suckmap = () => {
+	return (
+		<div className="suckmap">
+			<div className="card">
+				<div className="content">
+					<div className="heading">
+						<h1>&#60;/SUCKMAP&#62;</h1>
+						<h2>The Future Ahead</h2>
+					</div>
+
+					<p>1. $SUCK token launches</p>
+					<p>2. Suckiverse Whitepaper unveiled</p>
+					<p>3. Suckiverse website 2.0</p>
+					<p>4. 6969 generarive animated DilDAO NFTs are released</p>
+					<p>5. DilDAO fund for Teledildonics launches, with community decision over leadership and direction of the Suckiverse</p>
+					<p>
+						6. Official Teledildonics bounty program launches, funding research and development into the creation of physical Teledildonics
+						systems that use capacitive touch and other technologies to log sensations of the Sucker and replicate sensation to the Suckee
+					</p>
+					<p>
+						7. Suckiverse Worldwide Launches, a full blown Suck-to-earn Sex metaverse with $SUCK as the utility token, governed fully by
+						DilDAO
+					</p>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Suckmap
