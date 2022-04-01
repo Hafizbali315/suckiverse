0834e01e7d9d72a52a60f4c0676fd8075bf042a9
hafizbali315 <hafizbali315@gmail.com>
2022-04-02 01:57:11 +0500

    Mission Section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './components/Navbar'
+import Mission from './components/Mission'
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
+			<Mission />
 		</div>
 	)
 }
---
+++ src/components/Mission.js
@@ -0,0 +1,20 @@
+const Mission = () => {
+	return (
+		<div className="mission">
+			<h1>//Our Mission.</h1>
+
+			<p>
+				$Suck Token is launching to propel the first ever Suckiverse. You’ve heard of Software as a service, Play to Earn, and the
+				metaverse. Now get ready for the first ever Sucking as a Service metaverse project. The Suckiverse allows you to get sucked off in
+				the metaverse through advanced teledildonics that reward participants through Suck-to-Earn (S2E) blockchain mechanics.
+			</p>
+
+			<p>
+				A one of a kind revolutionary concept allowing people from all over the world to be sucked off at once, stimulating the global
+				economy.
+			</p>
+		</div>
+	)
+}
+
+export default Mission
